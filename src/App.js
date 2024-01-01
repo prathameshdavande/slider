a5b9dcc86820696e7409ad4dc3a41754a3f69ee2
Prathamesh Davande <prathamesh.pd.2000@gmail.com>
2024-01-01 22:21:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const images = [
+  "https://random.imagecdn.app/500/150",
+  "https://random.imagecdn.app/500/150",
+  "https://random.imagecdn.app/500/150",
+  // { imgId: 1, img: "https://random.imagecdn.app/500/150" },
+  // { imgId: 2, img: "https://random.imagecdn.app/500/150" },
+  // { imgId: 3, img: "https://random.imagecdn.app/500/150" },
+];
+
+export default function App() {
+  const [sliderImages, setSliderImages] = useState(images);
+
+  function handleArrowLeft() {
+    return setSliderImages(images.map((img, i) => i - 1));
+  }
+  function handleArrowRight() {
+    return setSliderImages(images.map((img, i) => i + 1));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Cards
+        sliderImages={sliderImages}
+        onArrowLeft={handleArrowLeft}
+        onArrowRight={handleArrowRight}
+      />
     </div>
   );
 }
 
-export default App;
+function Cards({ sliderImages, onArrowLeft, onArrowRight }) {
+  return (
+    <div className="cards">
+      {sliderImages.map((sliderImage, i) => (
+        <Card
+          sliderImage={sliderImage}
+          key={sliderImage.id}
+          onArrowLeft={onArrowLeft}
+          onArrowRight={onArrowRight}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Card({ sliderImage, onArrowLeft, onArrowRight }) {
+  return (
+    <div>
+      <div className="card">
+        <span onClick={onArrowLeft}>
+          <i class="gg-push-chevron-left"></i>
+        </span>
+        <img src={sliderImage.img} alt="abc" />
+        <span onClick={onArrowRight}>
+          <i class="gg-push-chevron-right"></i>
+        </span>
+      </div>
+    </div>
+  );
+}
