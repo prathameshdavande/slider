996d4cfce6a0f682e7fd95f83f07e9cd30cdee72
unknown <prathamesh.pd.2000@gmail.com>
2024-01-10 20:50:11 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,67 +1,9 @@
-import { useState } from "react";
-
-const images = [
-  // "https://random.imagecdn.app/500/150",
-  // "https://random.imagecdn.app/500/150",
-  // "https://random.imagecdn.app/500/150",
-  { imgId: 1, img: "https://random.imagecdn.app/500/150" },
-  // { imgId: 2, img: "https://random.imagecdn.app/500/150" },
-  // { imgId: 3, img: "https://random.imagecdn.app/500/150" },
-];
+import './index.css';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
 
 export default function App() {
-  const [sliderImages, setSliderImages] = useState(images);
-
-  function handleArrowLeft() {
-    return setSliderImages(images.map((img, i) => i - 1));
-  }
-  function handleArrowRight() {
-    return setSliderImages(images.map((img, i) => i + 1));
-  }
-
-  return (
-    <div className="app">
-      <Cards
-        sliderImages={sliderImages}
-        onArrowLeft={handleArrowLeft}
-        onArrowRight={handleArrowRight}
-      />
-    </div>
-  );
+  return <ImageSlider slides={SliderData} />;
 }
 
-function Cards({ sliderImages, onArrowLeft, onArrowRight }) {
-  return (
-    <div className="cards">
-      {sliderImages.map((sliderImage, i) => (
-        <div>
-          <span onClick={onArrowLeft} className="arrows">
-            <i class="gg-push-chevron-left"></i>
-          </span>
-          {/* <div className="card">
-            <img src={sliderImage.img} alt="abc" />
-          </div> */}
-          <span onClick={onArrowRight} className="arrows">
-            <i class="gg-push-chevron-right"></i>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-// function Card({ sliderImage, onArrowLeft, onArrowRight }) {
-//   return (
-//     <div>
-//       <div className="card">
-//         <span onClick={onArrowLeft}>
-//           <i class="gg-push-chevron-left"></i>
-//         </span>
-//         <img src={sliderImage.img} alt="abc" />
-//         <span onClick={onArrowRight}>
-//           <i class="gg-push-chevron-right"></i>
-//         </span>
-//       </div>
-//     </div>
-//   );
-// }
+// export default App;
